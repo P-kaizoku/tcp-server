454ea65f96d8164aeba431b4c136f66e44e87ef3
pabitra <pabitram322@gmail.com>
2026-06-28 21:05:04 +0530

    add a map with syncronized write nd read methods

--- main.go
+++ main.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 	"net"
 	"strings"
+	"sync"
+)
+
+var (
+	store = make(map[string]string)
+	mu    sync.RWMutex
 )
 
 func handle(conn net.Conn) {
@@ -11,25 +17,51 @@ func handle(conn net.Conn) {
 
 	buff := make([]byte, 1024)
 
-	n, err := conn.Read(buff)
-	if err != nil {
-		fmt.Println("Error in reading bytes", err)
-		return
-	}
+	for {
+		n, err := conn.Read(buff)
+		if err != nil {
+			fmt.Println("Error in reading bytes", err)
+		}
 
-	message := strings.TrimSpace(string(buff[:n]))
-	fmt.Println("Recieved:", message)
+		message := strings.Fields(strings.TrimSpace(string(buff[:n])))
+		fmt.Println("Recieved:", message)
 
-	if message == "PING" {
-		conn.Write([]byte("PING\r\n"))
-	} else {
-		conn.Write([]byte("Invalid command\r\n"))
+		switch message[0] {
+		case "PING":
+			conn.Write([]byte("PONG\r\n"))
+
+		case "SET":
+			key := message[1]
+			val := message[2]
+			if key == "" || val == "" {
+				fmt.Print("key or val is missing")
+				conn.Write([]byte("key or val is missing"))
+			}
+			mu.Lock()
+			store[key] = val
+			mu.Unlock()
+			conn.Write([]byte("Key set successfully\r\n"))
+
+		case "GET":
+			key := message[1]
+			mu.RLock()
+			val, ok := store[key]
+			mu.RUnlock()
+			if !ok {
+				conn.Write([]byte("Error: key not found"))
+			}
+			buff := []byte(val + "\r\n")
+			conn.Write(buff)
+		}
 	}
 
 }
 
 func main() {
-	listener, _ := net.Listen("localhost", ":8080")
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println("error occured in starting the server", err)
+	}
 
 	defer listener.Close()
 
