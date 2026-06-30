27ed3af4229d0726f2e0742f754d28c1776b1c64
pabitra <pabitram322@gmail.com>
2026-07-01 01:24:00 +0530

    implemented WAL to save state of the db.

--- main.go
+++ main.go
@@ -13,18 +13,33 @@ var (
 	store     = make(map[string]string)
 	taskQueue = make(chan string, 100)
 	mu        sync.RWMutex
+	walFile   *os.File
 )
 
-func 
+func load_data() {
+	data, err := os.ReadFile("data.log")
+	if err != nil {
+		return
+	}
+
+	logs := strings.Split(string(data), "\n")
+
+	for _, log := range logs {
+		if log == "" {
+			continue
+		}
+		parts := strings.Fields(log)
+
+		if len(parts) >= 3 && parts[0] == "SET" {
+			store[parts[1]] = strings.Join(parts[2:], " ")
+		}
+	}
+
+}
 
 func saveFile(key, val string) {
-	file, err := os.OpenFile("data.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	file.WriteString(fmt.Sprintf("SET %s %s", key, val))
+	walFile.WriteString(fmt.Sprintf("SET %s %s\n", key, val))
+	walFile.Sync()
 }
 
 func handle(conn net.Conn) {
@@ -47,18 +62,21 @@ func handle(conn net.Conn) {
 			conn.Write([]byte("PONG\r\n"))
 
 		case "SET":
-			key := message[1]
-			val := message[2]
-			if key == "" || val == "" {
-				fmt.Println("key or val is missing")
-				conn.Write([]byte("key or val is missing"))
-				continue
+			parts := strings.Fields(strings.TrimSpace(string(buff[:n])))
+
+			if len(parts) >= 3 && parts[0] == "SET" {
+				key := parts[1]
+				val := strings.Join(parts[2:], " ")
+
+				mu.Lock()
+				store[key] = val
+				mu.Unlock()
+
+				saveFile(key, val)
+				conn.Write([]byte("Key set successfully\r\n"))
+			} else {
+				conn.Write([]byte("Error: missing key or val\r\n"))
 			}
-			mu.Lock()
-			store[key] = val
-			mu.Unlock()
-			saveFile(key, val)
-			conn.Write([]byte("Key set successfully\r\n"))
 
 		case "GET":
 			key := message[1]
@@ -86,6 +104,13 @@ func handle(conn net.Conn) {
 
 func main() {
 
+	walFile, err := os.OpenFile("data.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	defer walFile.Close()
+
 	fmt.Printf("The workers are ready and waiting...")
 	for i := 1; i <= 3; i++ {
 		go func(workerID int) {
@@ -98,8 +123,6 @@ func main() {
 		}(i)
 	}
 
-	
-
 	port := ":8080"
 	if len(os.Args) > 1 {
 		port = ":" + os.Args[1]
@@ -111,6 +134,7 @@ func main() {
 	}
 
 	defer listener.Close()
+	load_data()
 
 	for {
 		conn, err := listener.Accept()
